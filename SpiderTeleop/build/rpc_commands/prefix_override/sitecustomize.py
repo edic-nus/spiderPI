b6e0a2211d55d4ccf8e789cd7db0e274653f554e
Teh Wei Sheng <wsjasonteh2003@gmail.com>
2024-07-17 16:37:17 +0700

    added navigation_demo code

--- SpiderTeleop/build/rpc_commands/prefix_override/sitecustomize.py
+++ SpiderTeleop/build/rpc_commands/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/wshengggg/HWSpider/install/rpc_commands'
+    sys.prefix = sys.exec_prefix = '/home/wshengggg/spiderPI/SpiderTeleop/install/rpc_commands'
